5b89f4769627201855c6bca1c064d227dd6b40f1
Bijay <bijay78082@gmail.com>
2026-06-26 16:26:23 +0530

    list variable

--- 06-terraform/terraform-rev/03-variable/sample.tf
+++ 06-terraform/terraform-rev/03-variable/sample.tf
@@ -1,7 +1,47 @@
+## plain variable
 variable "fruit_name" {
   default = "apple"
 }
 
 output "fruit_name" {
   value = var.fruit_name
+}
+
+## List variable
+variable "fruits" {
+  default = [
+    "apple",
+    "banana"
+  ]
+// default = [ "apple", "banana" ] // single line syntax
+}
+
+## map variable, plain
+
+variable "fruit_stock" {
+  default = {
+    apple = 100
+    banana = 200
+  }
+}
+
+## map variable, map of map
+
+variable "fruit_stock_with_price" {
+  default = {
+    apple = {
+      stock = 500
+      price = 5
+    }
+    banana = {
+      stock = 1000
+      price = 3
+    }
+  }
+}
+
+## Access a list variable, list index starts from zero
+
+output "fruit_first" {
+  value = var.fruits.[0]
 }
