2a7f8ba1a400c22ba9cc77b2d5ca44e2e1638134
Bijay <bijay78082@gmail.com>
2026-06-27 09:55:54 +0530

    variable

--- 06-terraform/terraform-rev/04-function/main.tf
+++ 06-terraform/terraform-rev/04-function/main.tf
@@ -6,3 +6,13 @@ output "fruit" {
  // value = var.fruits[2]
   value = element(var.fruits, 2)
 }
+
+variable "fruits_with_stock" {
+  default = {
+    apple = 100
+  }
+}
+
+output "fruit_stock" {
+  value = var.fruits_with_stock["banana"]
+}
