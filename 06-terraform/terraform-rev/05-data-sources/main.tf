a898e735b005b81f0ff93abc053a2be35df68a8c
Bijay <bijay78082@gmail.com>
2026-06-27 10:55:42 +0530

    data-source

--- 06-terraform/terraform-rev/05-data-sources/main.tf
+++ 06-terraform/terraform-rev/05-data-sources/main.tf
@@ -1,4 +1,5 @@
 data "aws_ami" "ami" {
-  most_recent      = true
-  name_regex       = "centos-*"
-  owners           = ["125523088429"]
+  most_recent = true
+  name_regex  = "centos-*"
+  owners = ["125523088429"]
+}
