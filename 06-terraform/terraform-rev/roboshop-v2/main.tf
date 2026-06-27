d4ad84fcdfe5ec52a4e18b197e497a8b5f22a6ae
Bijay <bijay78082@gmail.com>
2026-06-27 12:16:24 +0530

    using variable in roboshop

--- 06-terraform/terraform-rev/roboshop-v2/main.tf
+++ 06-terraform/terraform-rev/roboshop-v2/main.tf
@@ -1,7 +1,27 @@
+variable "ami" {
+  default = "ami-027e79d2df3347114"
+}
+
+variable "instance_type" {
+  default = "t3.small"
+}
+
+variable "security_groups" {
+  default = ["sg-0031efde6a51cea49"]
+}
+
+variable "zone_id" {
+  default = "Z0263363ANDXUHDABKMS"
+}
+
+
+
+
+
 resource "aws_instance" "frontend" {
-  ami           = "ami-027e79d2df3347114"
-  instance_type = "t3.small"
-  vpc_security_group_ids = [ "sg-0031efde6a51cea49" ]
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_groups
 
   tags = {
     Name = "frontend"
@@ -9,7 +29,7 @@ resource "aws_instance" "frontend" {
 }
 
 resource "aws_route53_record" "frontend" {
-  zone_id = "Z0263363ANDXUHDABKMS"
+  zone_id = var.zone_id
   name    = "frontend-dev.lerntechnology.online"
   type    = "A"
   ttl     = 30
