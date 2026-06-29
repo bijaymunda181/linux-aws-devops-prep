b3498a257aab501ee07c21d7756493549f97c0af
Bijay <bijay78082@gmail.com>
2026-06-29 13:48:43 +0530

    for_each loop

--- 06-terraform/terraform-rev/06-loops/for-each/main.tf
+++ 06-terraform/terraform-rev/06-loops/for-each/main.tf
@@ -9,6 +9,6 @@ variable "components" {
 
 resource "aws_security_group" "allow_tls" {
   for_each = var.components
- // name = lookup(var.components,each.value["name"], null )
-  name = each.value["name"]
+  name = lookup(var.components,each.value["name"], null )
+  // name = each.value["name"]
 }
