5e263637f61139694ce6bf53339decc5aff730d9
Bijay <bijay78082@gmail.com>
2026-06-29 14:42:10 +0530

    for_each loop

--- 06-terraform/terraform-rev/roboshop-v2/main.tf
+++ 06-terraform/terraform-rev/roboshop-v2/main.tf
@@ -14,180 +14,41 @@ variable "zone_id" {
   default = "Z0263363ANDXUHDABKMS"
 }
 
+variable "components" {
+  default = {
+    frontend = { name = "frontend-dev"}
+    mongodb = {name = "mongodb-dev"}
+    catalogue = {name = "catalogue-dev"}
+    user = {name = "user-dev"}
+    redis = {name = "redis-dev"}
+    cart = {name = "cart-dev"}
+    mysql = {name = "mysql-dev"}
+    shipping = {name = "shipping-dev"}
+    payment = {name = "payment-dev"}
+    rabbitmq = {name = "rabbitmq-dev"}
+  }
+}
 
 
 
 
-resource "aws_instance" "frontend" {
+resource "aws_instance" "instance" {
+  for_each = var.components
   ami           = var.ami
   instance_type = var.instance_type
   vpc_security_group_ids = var.security_groups
 
   tags = {
-    Name = "frontend"
+    Name = lookup(each.key["name"], null)
   }
 }
 
-resource "aws_route53_record" "frontend" {
-  zone_id = var.zone_id
-  name    = "frontend-dev.lerntechnology.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.frontend.private_ip]
-}
+# resource "aws_route53_record" "record" {
+#   for_each = var.components
+#   zone_id = var.zone_id
+#   name    = "frontend-dev.lerntechnology.online"
+#   type    = "A"
+#   ttl     = 30
+#   records = [lookup(aws_instance.instance, each.key)]
+# }
 
-resource "aws_instance" "mongodb" {
-  ami           = "ami-027e79d2df3347114"
-  instance_type = "t3.small"
-  vpc_security_group_ids = [ "sg-0031efde6a51cea49" ]
-
-  tags = {
-    Name = "mongodb"
-  }
-}
-
-resource "aws_route53_record" "mongodb" {
-  zone_id = "Z0263363ANDXUHDABKMS"
-  name    = "mongodb-dev.lerntechnology.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mongodb.private_ip]
-}
-
-resource "aws_instance" "catalogue" {
-  ami           = "ami-027e79d2df3347114"
-  instance_type = "t3.small"
-  vpc_security_group_ids = [ "sg-0031efde6a51cea49" ]
-  tags = {
-    Name = "catalogue"
-  }
-}
-
-resource "aws_route53_record" "catalogue" {
-  zone_id = "Z0263363ANDXUHDABKMS"
-  name    = "catalogue-dev.lerntechnology.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.catalogue.private_ip]
-}
-
-
-resource "aws_instance" "cart" {
-  ami           = "ami-027e79d2df3347114"
-  instance_type = "t3.small"
-  vpc_security_group_ids = [ "sg-0031efde6a51cea49" ]
-  tags = {
-    Name = "cart"
-  }
-}
-
-resource "aws_route53_record" "cart" {
-  zone_id = "Z0263363ANDXUHDABKMS"
-  name    = "cart-dev.lerntechnology.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.cart.private_ip]
-}
-
-
-resource "aws_instance" "redis" {
-  ami           = "ami-027e79d2df3347114"
-  instance_type = "t3.small"
-  vpc_security_group_ids = [ "sg-0031efde6a51cea49" ]
-  tags = {
-    Name = "redis"
-  }
-}
-
-resource "aws_route53_record" "redis" {
-  zone_id = "Z0263363ANDXUHDABKMS"
-  name    = "redis-dev.lerntechnology.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.redis.private_ip]
-}
-
-resource "aws_instance" "user" {
-  ami           = "ami-027e79d2df3347114"
-  instance_type = "t3.small"
-  vpc_security_group_ids = [ "sg-0031efde6a51cea49" ]
-  tags = {
-    Name = "user"
-  }
-}
-
-resource "aws_route53_record" "user" {
-  zone_id = "Z0263363ANDXUHDABKMS"
-  name    = "user-dev.lerntechnology.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.user.private_ip]
-}
-
-resource "aws_instance" "mysql" {
-  ami           = "ami-027e79d2df3347114"
-  instance_type = "t3.small"
-  vpc_security_group_ids = [ "sg-0031efde6a51cea49" ]
-  tags = {
-    Name = "mysql"
-  }
-}
-
-resource "aws_route53_record" "mysql" {
-  zone_id = "Z0263363ANDXUHDABKMS"
-  name    = "mysql-dev.lerntechnology.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mysql.private_ip]
-}
-
-resource "aws_instance" "shipping" {
-  ami           = "ami-027e79d2df3347114"
-  instance_type = "t3.small"
-  vpc_security_group_ids = [ "sg-0031efde6a51cea49" ]
-  tags = {
-    Name = "shipping"
-  }
-}
-
-resource "aws_route53_record" "shipping" {
-  zone_id = "Z0263363ANDXUHDABKMS"
-  name    = "shipping-dev.lerntechnology.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.shipping.private_ip]
-}
-
-resource "aws_instance" "rabbitmq" {
-  ami           = "ami-027e79d2df3347114"
-  instance_type = "t3.small"
-  vpc_security_group_ids = [ "sg-0031efde6a51cea49" ]
-  tags = {
-    Name = "rabbitmq"
-  }
-}
-
-resource "aws_route53_record" "rabbitmq" {
-  zone_id = "Z0263363ANDXUHDABKMS"
-  name    = "rabbitmq-dev.lerntechnology.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.rabbitmq.private_ip]
-}
-
-resource "aws_instance" "payment" {
-  ami           = "ami-027e79d2df3347114"
-  instance_type = "t3.small"
-  vpc_security_group_ids = [ "sg-0031efde6a51cea49" ]
-  tags = {
-    Name = "payment"
-  }
-}
-
-resource "aws_route53_record" "payment" {
-  zone_id = "Z0263363ANDXUHDABKMS"
-  name    = "payment-dev.lerntechnology.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.payment.private_ip]
-}
