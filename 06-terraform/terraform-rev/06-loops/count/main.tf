bc1dbbbfebc6baac94cd96b63ab63a14b53f7291
Bijay <bijay78082@gmail.com>
2026-06-29 12:05:12 +0530

    loops

--- 06-terraform/terraform-rev/06-loops/count/main.tf
+++ 06-terraform/terraform-rev/06-loops/count/main.tf
@@ -3,13 +3,16 @@ variable "components" {
 }
 
 resource "aws_instance" "instance" {
+
   count = length(var.components)
+
   ami           = "ami-027e79d2df3347114"
   instance_type = "t3.small"
   vpc_security_group_ids = [ "sg-0031efde6a51cea49" ]
 
 
 tags = {
-  Name = ""
+  //Name = var.components[count.index]
+  Name = element(var.components, count.index )
 }
 }
