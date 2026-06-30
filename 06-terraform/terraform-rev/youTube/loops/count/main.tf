4c000316f35021784c335e2ad0b7f029ff62d9a6
Bijay <bijay78082@gmail.com>
2026-06-30 09:39:54 +0530

    count

---
+++ 06-terraform/terraform-rev/youTube/loops/count/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.54.1"
+    }
+  }
+}
+variable "user_names" {
+  default = ["user1", "user2", "user3"]
+}
+
+resource "aws_iam_user" "user_name" {
+  count = length(var.user_names)
+  name = var.user_names[count.index]
+}
