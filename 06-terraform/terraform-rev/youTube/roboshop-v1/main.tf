f7bbcc0f52ac34d50a04c46503bc5b508b7bd08e
Bijay <bijay78082@gmail.com>
2026-06-30 12:58:12 +0530

    roboshop

---
+++ 06-terraform/terraform-rev/youTube/roboshop-v1/main.tf
@@ -0,0 +1,47 @@
+variable "ami" {
+  default = "ami-027e79d2df3347114"
+}
+
+variable "instance_type" {
+  default = "t3.small"
+}
+
+variable "security_group" {
+  default = [ "sg-0031efde6a51cea49" ]
+}
+
+variable "zone_id" {
+  default = "Z0263363ANDXUHDABKMS"
+}
+
+variable "components" {
+  default = {
+    frontend  = {name = "frontend-dev"}
+    mongodb  = {name = "mongodb-dev"}
+    catalogue  = {name = "catalogue-dev"}
+    cart  = {name = "cart-dev"}
+    redis  = {name = "rdis-dev"}
+  }
+}
+
+
+resource "aws_instance" "instance" {
+  for_each = var.components
+
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_group
+
+  tags = {
+    Name = lookup(each.value, "name", null )
+  }
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z0263363ANDXUHDABKMS"
+  name    = ${lookup(each.value, "name", null ).lerntechnology.online}
+  type    = "A"
+  ttl     = 30
+  records = [ aws_instance.instance[each.key].private_ip ]
+}
+
