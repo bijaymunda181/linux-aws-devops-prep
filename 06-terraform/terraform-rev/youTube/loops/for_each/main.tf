6ab495eff93bc978ff682d0bd0f34081f7888f35
Bijay <bijay78082@gmail.com>
2026-06-30 10:07:05 +0530

    for_each

---
+++ 06-terraform/terraform-rev/youTube/loops/for_each/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.54.1"
+    }
+  }
+}
+variable "iam_users" {
+  default = ["Rahul", "Mohan", "Sohan"]
+}
+
+resource "aws_iam_user" "users" {
+  for_each = var.iam_users
+  name = each.value
+}
