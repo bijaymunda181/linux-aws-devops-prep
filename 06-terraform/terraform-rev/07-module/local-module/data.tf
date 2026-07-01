cef632e537383c2119147ab80dea6772c7c51caf
Bijay <bijay78082@gmail.com>
2026-07-01 10:41:05 +0530

    module

--- 06-terraform/terraform-rev/07-module/local-module/data.tf
+++ 06-terraform/terraform-rev/07-module/local-module/data.tf
@@ -1,8 +1,23 @@
-data "aws_ami_ids" "ami" {
-  name_regex  = "CentOS-*"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.54.1"
+    }
+  }
+}
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "CentOS Stream 9 x86_64 20260526"
   owners = ["125523088429"]
 }
 
 output "ami_ids" {
-  value = data.aws_ami_ids.ami
+  value = data.aws_ami.ami
 }
+
+resource "aws_instance" "instance" {
+
+  ami           = data.aws_ami.ami.id
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0031efde6a51cea49" ]
