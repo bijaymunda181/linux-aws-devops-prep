f5e77909699f6061899db9c85f70cbe141dc7903
Bijay <bijay78082@gmail.com>
2026-07-02 10:23:13 +0530

    provisioners

--- 06-terraform/terraform-rev/roboshop-v3/module/main.tf
+++ 06-terraform/terraform-rev/roboshop-v3/module/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.2"
+    }
+  }
+}
 resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.id
   instance_type = var.instance_type
@@ -5,9 +13,6 @@ resource "aws_instance" "instance" {
 
   tags = {
     Name = var.name
-  }
-  provisioner "local-exec" {
-    command = "sleep 30"
   }
 }
 
@@ -22,5 +27,20 @@ resource "aws_route53_record" "record" {
   records = [aws_instance.instance.private_ip]
 }
 
+resource "null_resource" "ansible" {
+
+  depends_on = [
+    aws_route53_record.record
+  ]
+  provisioner "local-exec" {
+    command = <<EOF
+cd /root/roboshop-ansible
+git pull
+sleep 30
+ansible-playbook -i ${var.name}-dev.lerntechnology.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e
+component=${var.name}
+EOF
+  }
+}
 
 
