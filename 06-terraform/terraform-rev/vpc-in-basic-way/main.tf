699f4b488abf3f16b9743b5ed94a5468274d8100
Bijay <bijay78082@gmail.com>
2026-08-11 16:20:10 +0530

    terraform

--- 06-terraform/terraform-rev/vpc-in-basic-way/main.tf
+++ 06-terraform/terraform-rev/vpc-in-basic-way/main.tf
@@ -3,15 +3,15 @@ variable "cidr" {
 }
 
 variable "public_subnets" {
-  default = ["10.0.0.0/24", 10.0.1.0/24]
+  default = ["10.0.0.0/24", "10.0.1.0/24"]
 }
 
 variable "app_subnets" {
-  default = ["10.0.3.0/24", 10.0.4.0/24]
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "db_subnets" {
-  default = ["10.0.5.0/24", 10.0.6.0/24]
+  default = ["10.0.5.0/24", "10.0.6.0/24"]
 }
 
 
