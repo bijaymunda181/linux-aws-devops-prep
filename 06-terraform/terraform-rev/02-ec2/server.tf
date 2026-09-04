01a0b71661231e401945b7bcbf4efedec0f5548d
Bijay <bijay78082@gmail.com>
2026-09-04 11:54:52 +0530

    terraform

--- 06-terraform/terraform-rev/02-ec2/server.tf
+++ 06-terraform/terraform-rev/02-ec2/server.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~>6.0"
+ }
+}
+}
 resource "aws_instance" "example" {
   ami           = "ami-027e79d2df3347114"
   instance_type = "t3.micro"
