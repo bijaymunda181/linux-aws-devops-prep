cda5d7693ec374c25275393e8e718c9919718dac
Bijay <bijay78082@gmail.com>
2026-09-05 12:32:29 +0530

    terraform

--- 06-terraform/terraform-rev/02-ec2/server.tf
+++ 06-terraform/terraform-rev/02-ec2/server.tf
@@ -14,7 +14,7 @@ data "aws_security_group" "example" {
 resource "aws_instance" "example" {
   ami           = "ami-081b0a6eac00b4f53"
   instance_type = "t3.micro"
-  vpc_security_group_ids = data.aws_security_group.example.id
+  vpc_security_group_ids = [data.aws_security_group.example.id]
 
   tags = {
     Name = "HelloWorld"
