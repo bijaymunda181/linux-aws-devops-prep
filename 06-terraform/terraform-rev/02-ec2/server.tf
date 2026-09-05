96795096ed4a4b6eefe61d58b7b7be81031b1d64
Bijay <bijay78082@gmail.com>
2026-09-05 12:18:29 +0530

    terraform

--- 06-terraform/terraform-rev/02-ec2/server.tf
+++ 06-terraform/terraform-rev/02-ec2/server.tf
@@ -9,6 +9,7 @@ terraform {
 resource "aws_instance" "example" {
   ami           = "ami-081b0a6eac00b4f53"
   instance_type = "t3.micro"
+  security_groups = ["sg-0c6aa0d3a39bac0df"]
 
   tags = {
     Name = "HelloWorld"
