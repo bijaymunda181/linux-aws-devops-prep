e4445f8f7e1ad65ef55da4d3282b299f9b95877f
Bijay <bijay78082@gmail.com>
2026-09-05 12:37:01 +0530

    terraform

--- 06-terraform/terraform-rev/02-ec2/server.tf
+++ 06-terraform/terraform-rev/02-ec2/server.tf
@@ -7,14 +7,14 @@ terraform {
 }
 }
 
-data "aws_security_group" "example" {
-  name = "my-sg"
+data "aws_security_group" "launch-wizard-1" {
+  name = "launch-wizard-1"
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-081b0a6eac00b4f53"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [data.aws_security_group.example.id]
+  vpc_security_group_ids = [data.aws_security_group.launch-wizard-1.id]
 
   tags = {
     Name = "HelloWorld"
